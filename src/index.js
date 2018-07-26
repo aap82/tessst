52fcdd49f5b14b66c9538481ccccc6a1fafd8c99
amit p <aapatel115@gmail.com>
2018-07-25 21:34:12 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import "./styles.css";
+import "./styles.css"
 
+class DatesForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date(2018, 7, 1),
+    }
+  }
+
+  setDate = date => {
+    console.log(date)
+  }
+
+  render() {
+    console.log("a", this.state.date)
+    return (
+      <div>
+        <input type="date" onChange={this.setDate} />
+      </div>
+    )
+  }
+}
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <DatesForm />
     </div>
-  );
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
